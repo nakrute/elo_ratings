aa6ddfe03a9f5cbcba1c87c88c6ebee0f77fe534
nakrute <70236119+nakrute@users.noreply.github.com>
2020-10-21 22:19:18 -0400

    Update elo_ratings.py

--- NFL/elo_ratings.py
+++ NFL/elo_ratings.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from random import random 
+from random import random
 
 #K is the standard change per game
 K = 20
@@ -52,12 +52,23 @@ class nfl_data(object):
     def read_schedule(self, file):
         global schedule
         schedule = pd.read_csv(file)
-        schedule = schedule.set_index("TEAM")        
+        schedule = schedule.set_index("TEAM")
+
+    def read_scores(self, file):
+        global scores
+        scores = pd.read_csv(file)
+        scores = scores.set_index("Teams")
 
     #return the elo we are storing for now
     def get_elo(self, team, week):
         elo = data.loc[team, week]
         return elo
+
+    #set the elo for a team
+    def set_elo(self, team, week, elo):
+        column_idx = data.columns.get_loc(week) + 1
+        next_week = data.columns.values[column_idx]
+        data.loc[team, next_week] = elo        
 
     #get team abreviation
     def get_abv(self, team):
@@ -93,6 +104,26 @@ class nfl_data(object):
         opponent = schedule.loc[team_abv, week]
         opponent = self.get_team_abv(opponent)
         return opponent
+
+    #get the average score of the team
+    def get_average_score(self, team):
+        avg_score = scores.loc[team, "Average"]
+        return avg_score
+
+    #set the predicted scores for the week
+    def set_predicted_score(self, team, week, score):
+        column_idx = scores.columns.get_loc(week)
+        next_week = scores.columns.values[column_idx]
+        scores.loc[team, next_week] = score
+
+    #get the score of a team for a specific week
+    def get_score(self, team, week):
+        score = scores.loc[team, week]
+        return score
+
+    #function to write the file
+    def write_file(self, dataframe, file_name):
+        dataframe.to_csv(file_name)
 
 class elo_ratings(nfl_data):
     #calculate the percentage chance a team will win
@@ -134,7 +165,8 @@ class elo_ratings(nfl_data):
         spread = round(diff/25,2)
         return spread
 
-    #make adjustments
+    #make adjustments #TODO
+    #this part is important since it adjusts the teams to see if they have a better chance of winning or not
     def adjustments(self, elo, home):
         if home_away == "H":
             elo += home_field_advantage
@@ -169,7 +201,31 @@ class elo_ratings(nfl_data):
                 print("The", team, "are playing the", opp, "in", week, "and are", home_away)
                 print("The", teams_elo[better_team], "have a", odds_percent,
                         "% chance of winning, with a spread of", spread,"\n")
-                    
+
+    #calculate the brier score for accuracy
+    #1 for win-loss if it's right
+    #0 for win_loss if it's wrong
+    def brier_score(chance, odds, win_loss):
+        return (odds-win_loss)**2
+
+       #predict the scores for the team
+    def get_predicted_score(self, team, week, runs = 1000, plot=False):
+        opp = elo.get_opponent(team, week)
+        if opp == "Bye":
+            print("The", team, "are on a bye this week.")
+        else:
+            elo_team = elo.get_elo(team, week)
+            elo_opp = elo.get_elo(opp, week)
+            spread = self.point_spread(elo_team, elo_opp)
+            score = np.random.normal(spread, standard_deviation, runs)
+            avg = self.get_average_score(team)
+            predicted_score = avg + score
+            predicted_score = int(round(np.mean(predicted_score),0))
+            if plot:
+                _ = plt.hist(score)
+                plt.show()
+            return predicted_score
+
 #simulator for each week
 class simulator(elo_ratings):
     def run_week(self, week, simulate=True):
@@ -200,35 +256,26 @@ class simulator(elo_ratings):
             else:
                 print("Simulating for the", team, "we get a winning chance of", chance,"%")
 
-    def get_predicted_score(self, team, week, runs = 100):
-        opp = elo.get_opponent(team, week)
-        if opp == "Bye":
-            print("The", team, "are on a bye this week.\n")
-        else:
-            elo_team = elo.get_elo(team, week)
-            elo_opp = elo.get_elo(opp, week)
-            spread = self.point_spread(elo_team, elo_opp)
-            score = np.random.normal(spread, standard_deviation, runs)
-            print(score)
-            print(np.mean(score))
+    #calculate the scores for each game of the week
+    def simulate_week_with_scores(self, week, write=False):
+        teams = data.index
+        for team in teams:
+            predicted_score = self.get_predicted_score(team, week)
+            print("Predicting the", team, "will score", predicted_score, "points in", week, ".")
+            if write == True:
+                if predicted_score == "None":
+                    predicted_score = "BYE"
+                self.set_predicted_score(team, week, predicted_score)
             
 #create object
 elo = simulator()
 #set up global data
 elo.read_and_clean("D:/Elo_Ratings/elo_data.csv")
 elo.read_schedule("D:/Elo_Ratings/schedule.csv")
-'''
+elo.read_scores("D:/Elo_Ratings/scores.csv")
 #testing code here
-week = "Week 6"
-team = "Eagles"
-opp = elo.get_opponent(team, week)
-elo_team = elo.get_elo(team, week)
-elo_opp = elo.get_elo(opp, week)
-print(elo.change_elo(elo_team, elo_opp, "A", 20, 19))
-elo.print_game_details(week, team)
-'''
-#elo.run_week("Week 6")
-#elo.simulate_games("Ravens", "Week 6")
-print(elo.get_predicted_score("Ravens", "Week 6"))
+#elo.run_week("Week 7")
+elo.simulate_week_with_scores("Week 7", write=True))
+                                       
 
 
